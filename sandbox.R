54d31e7d720edf8603287469fa6719d659abe16e
Anish Shah <asshah4@users.noreply.github.com>
2022-11-25 12:40:15 -0600

    add sf and ggplot2 dependencies; add compact AV node and CFB #2

--- sandbox.R
+++ sandbox.R
@@ -1,19 +1,21 @@
 library(sf)
 library(tidyverse)
 
-extent <- st_read("mapping/right_atrium_rao/cardiac_extent.shp")
-atrium <- st_read("mapping/right_atrium_rao/chamber_space.shp")
-conduction <- st_read("mapping/right_atrium_rao/conduction_system.shp")
-muscles <- st_read("mapping/right_atrium_rao/muscular_extensions.shp")
-septum <- st_read("mapping/right_atrium_rao/septal_structures.shp")
-valves <- st_read("mapping/right_atrium_rao/valvular_structures.shp")
-vessels <- st_read("mapping/right_atrium_rao/vessel_structures.shp")
+extent = st_read("atlas/right_atrium_rao/extent.shp")
+chambers = st_read("atlas/right_atrium_rao/chambers.shp")
+conduction = st_read("atlas/right_atrium_rao/conduction.shp")
+projections = st_read("atlas/right_atrium_rao/projections.shp")
+septum = st_read("atlas/right_atrium_rao/septum.shp")
+valves = st_read("atlas/right_atrium_rao/valves.shp")
+vessels = st_read("atlas/right_atrium_rao/vessels.shp")
 
 
 
 ggplot() +
-	geom_sf(data = atrium) +
+	geom_sf(data = chambers) +
 	geom_sf(data = vessels) +
 	geom_sf(data = septum) +
-	geom_sf(data = muscles) +
+	geom_sf(data = valves) +
+	geom_sf(data = projections) +
+	geom_sf(data = conduction) +
 	theme_void()
